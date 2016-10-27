c2e7205553e3cf15781c45ea05fb1e9a4ef3f9ac
wdj20101010 <wdj20101010@aliyun.com>
2016-10-27 07:36:41 -0500

    Create DialogEnter.java

---
+++ WdjPieceForAndroid-1.1-2016.10.27/DialogEnter.java
@@ -0,0 +1,64 @@
+package com.wdjpiece.android;
+
+import android.app.*;
+import android.view.*;
+import android.widget.*;
+import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Looper;
+
+public class DialogEnter extends Dialog
+{
+	public static String DialogEnterStr=null;
+	private Handler handler;
+	private EditText edittext;
+
+	public DialogEnter(Context context,String str)
+    {
+        super(context);
+        setContentView(R.layout.enter);
+
+		DialogEnter.this.setTitle("Enter");
+		edittext=(EditText)findViewById(R.id.enter);
+		edittext.setText(str);
+
+		//取消键
+		Button button1=(Button)findViewById(R.id.enterbutton1);
+		button1.setOnClickListener(new Button.OnClickListener(){
+				public void onClick(View v){
+					System.exit(0);
+				}
+			});
+
+		//确认键
+		Button button2=(Button)findViewById(R.id.enterbutton2);
+		button2.setOnClickListener(new Button.OnClickListener(){
+				public void onClick(View v){
+					DialogEnterStr=edittext.getText().toString();
+					DialogEnter.this.dismiss();
+				}
+			});
+	}
+
+	@Override
+	public void show()
+	{
+		handler=new Handler(){
+			@Override
+			public void handleMessage(Message mesg){
+				throw new RuntimeException();
+			}
+		};
+		super.show();
+		try{Looper.getMainLooper().loop();}
+		catch(RuntimeException e){}
+	}
+
+	@Override
+	public void dismiss()
+	{
+		super.dismiss();
+		handler.sendMessage(handler.obtainMessage());
+	}
+}
